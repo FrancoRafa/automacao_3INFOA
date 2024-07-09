7149b5222c3cfa9dca34295c663ceda484d19aca
Rafaela <rafaelafranco672@gmail.com>
2024-07-09 16:55:17 -0300

    semana8

--- semana8/dino.py
+++ semana8/dino.py
@@ -1,10 +1,26 @@
 import pyautogui
 
+def obterEstadoCenario():
+     if pyautogui.pixelMatchesColor(100,600,[255,255,255],50):
+          return 'DIA'
+     else
+          return 'NOITE'
+
 #clica no chrome
 pyautogui.click(200, 422, duration=.5)
 pyautogui.keyDown('space')
 while True:
-     if not pyautogui.pixelMatchesColor(200, 422, [255,255,255], tolerance=0.1):
+#pega cor dino
+    #corDino= pyautogui.pixel(70,718)
+     if obterEstadoCenario())== DIA:
+           if pyautogui.pixelMatchesColor(235,439,[83,83,83], 50) 
+           print(pyautogui.pixel(235,439))
+           pyautogui.keyDown('space')
+   
+     else
+          if  pyautogui.pixelMatchesColor(235, 439, [172,172,172]):
           pyautogui.keyDown('space')
+    
+
 
 
